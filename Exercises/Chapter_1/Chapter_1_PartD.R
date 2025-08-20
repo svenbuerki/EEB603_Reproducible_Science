17f0a545acde91b9ca3a2b14d7426b936486effe
svenbuerki <svenbuerki@boisestate.edu>
2025-08-20 10:11:03 -0600

    Updated website for Fall 2025

--- Exercises/Chapter_1/Chapter_1_PartD.R
+++ Exercises/Chapter_1/Chapter_1_PartD.R
@@ -51,7 +51,23 @@ exp_number(base = 2, power = 4)
 exp_number(base = "hello", power = 4)
 
 
-
+# Obj: Define a power function (exp_number): y = x^n
+# - Arguments: base (= x) and power (= n)
+# - Output: exp (= y) 
+# x, n and y are numbers
+exp_number <- function(base, power){
+  #Test if base or (|) power are different than numeric
+  if(class(base) != "numeric" | class(power) != "numeric"){
+    #If not numeric then stop and print error
+    stop("Both base and power MUST be numeric!")
+  }
+  
+  #Infer exp (= y) based on base and power
+  exp <- base^power
+  
+  #Return exp
+  return(exp)
+}
 
 
 
